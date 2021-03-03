d57289dc61ce4fa0d4ab5c13fb27f3e3db871306
Moubarik <Moubarik20@gmail.com>
2021-03-03 08:17:47 -0300

    All Done
    Work Done

--- atelier 2/Java.js
+++ atelier 2/Java.js
@@ -7,13 +7,53 @@ var Slide = document.querySelectorAll(".Photos");
 
 Slide.forEach(Photo => Photo.addEventListener('click' ,show));
 function show(){
-    this.classList.toggle("Overview")
-};
+    this.classList.toggle("Overview");
+}
 
- 
-// var Slide = document.getElementsByTagName(".P");
+Slide.forEach(Photo => Photo.addEventListener('click' ,mouve));
+function mouve(){
+    this.classList.toggle("Getin");
+}
 
-// Slide.forEach(Element => Element.addEventListener('' ,Getout));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var  Slide=document.querySelectorAll(".Photos")
+// Slide.forEach(Photo => Photo.addEventListener('click' ,show));
+// function alpha(){
+//     this.classList.toggle("Outover");
+// };
+
+
+
+
+
+
+    
+
+// var Slide = document.querySelector(".P");
+
+// Slide.forEach(Element => Element.addEventListener('click' ,Getout));
 // function Getout(){
 // this.classList.toggle("Outover")
 // };
