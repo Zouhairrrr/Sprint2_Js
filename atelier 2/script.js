4eabd6fbc01e9b0b1b2544bc9ddbc06b0f88ab18
Hassan Benhzaine <hassanbenhzaine@users.noreply.github.com>
2021-03-03 09:17:22 +0100

    ss

--- atelier 2/script.js
+++ atelier 2/script.js
@@ -1,13 +1,6 @@
 // Select all html elements within class ".box" and stock them in an array "panels"
 const panels = document.querySelectorAll('.box')
 
-function growBox(){
-        this.classList.toggle("boxgrow")
-};
-function textreset(){
-        this.classList.toggle("textreset")
-}
-
 // for each item in array add event listener 
-panels.forEach(item => item.addEventListener("click", growBox));
-panels.forEach(item => item.addEventListener("transitionend", textreset));
+panels.forEach(item => item.addEventListener("click", function(){this.classList.toggle("boxgrow")}));
+panels.forEach(item => item.addEventListener("transitionend", function(){this.classList.toggle("textreset")}));
